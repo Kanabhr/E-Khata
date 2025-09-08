2bbef49230bf72cfa88f6a17d836606c18f44433
Kanabhr <122717460+Kanabhr@users.noreply.github.com>
2025-09-08 22:22:10 +0530

    Main page Updated

--- app/page.jsx
+++ app/page.jsx
@@ -1,10 +1,14 @@
 import TopicList from "@/components/Topic-list";
+import PieChartComp from "@/components/piechart";
 
-export default function Home() {
+export default function HomePage() {
   return (
-    <div>
-      <TopicList />
-    </div>
+    <main className="p-6 bg-gray-100 min-h-screen">
+      <div className="grid gap-6 lg:grid-cols-2">
+        <TopicList />
+        <PieChartComp />
+      </div>
+    </main>
   );
 }
 //passwordfordb: pfz0Ubprl5IWKBCI
